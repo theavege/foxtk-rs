d6030807814db7322a14bf80ad0ce6e22eee7104
Artem Ageev <artem.v.ageev@gmail.com>
2026-04-08 20:12:15 +0300

    build

--- foxtk-sys/build.rs
+++ foxtk-sys/build.rs
@@ -2,14 +2,15 @@ use std::env;
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
-use glob;
+
+const DIST: &str = "fox-1.6.59";
 
 #[cfg(target_os = "linux")]
 fn main() {
+    let zip_url = format!("http://fox-toolkit.org/ftp/{DIST}.zip");
     let out_dir = env::var("OUT_DIR").unwrap();
-    let zip_url = "http://fox-toolkit.org/ftp/fox-1.6.59.zip";
-    let zip_path = Path::new(&out_dir).join("fox-1.6.59.zip");
-    let extract_dir = Path::new(&out_dir).join("fox-1.6.59");
+    let zip_path = Path::new(&out_dir).join(format!("{DIST}.zip"));
+    let extract_dir = Path::new(&out_dir).join(DIST);
 
     if !extract_dir.exists() {
         let response = reqwest::blocking::get(zip_url).expect("Failed to download fox zip");
@@ -21,35 +22,23 @@ fn main() {
     let mut source_paths = Vec::new();
     for entry in glob::glob(&format!("{}/src/*.cpp", extract_dir.display())).expect("Failed to read glob pattern") {
         match entry {
-            Ok(path) => {
-                let filename = path.file_name().unwrap().to_string_lossy();
-                if filename != "vsscanf.cpp" {
-                    source_paths.push(path);
-                };
-            }
-            Err(e) => println!("cargo:warning=Glob error: {:?}", e),
+            Ok(path) => source_paths.push(path),
+            Err(e) => eprintln!("cargo:warning=Glob error: {:?}", e),
         }
     }
+    const CAPI: &str = "cfoxtk/foxtk.cpp";
     cc::Build::new()
         .cpp(true)
         .flag_if_supported("-std=c++14")
         .includes([extract_dir.join("include")])
         .files(source_paths)
-        .compile("fox");
-
-    let mut include_paths = Vec::from(["-Icfoxtk".to_string()]);
-    include_paths.push(format!("-I{}", extract_dir.join("include").display()));
-    const CAPI: &str = "cfoxtk/foxtk.cpp";
-    cc::Build::new()
-        .cpp(true)
-        .flag_if_supported("-std=c++14")
         .flags(&include_paths)
         .file(CAPI)
         .compile("cfoxtk");
     println!("cargo:rerun-if-changed={CAPI}");
     bindgen::Builder::default()
         .header("cfoxtk/foxtk.h")
-        .clang_args(&include_paths)
+        .clang_args(&["-Icfoxtk".to_string(), format!("-I{}", extract_dir.join("include").display())])
         .generate()
         .expect("Unable to generate bindings")
         .write_to_file(
@@ -60,14 +49,14 @@ fn main() {
 
 #[cfg(target_os = "windows")]
 fn main() {
+    let zip_url = format!("http://fox-toolkit.org/ftp/{DIST}.zip");
     let out_dir = env::var("OUT_DIR").unwrap();
-    let zip_url = "http://fox-toolkit.org/ftp/fox-1.6.59.zip";
-    let zip_path = Path::new(&out_dir).join("fox-1.6.59.zip");
-    let extract_dir = Path::new(&out_dir).join("fox-1.6.59");
+    let zip_path = Path::new(&out_dir).join(format!("{DIST}.zip"));
+    let extract_dir = Path::new(&out_dir).join(DIST);
 
     if !extract_dir.exists() {
-        let response = reqwest::blocking::get(zip_url).expect("Failed to download fox zip");
         let mut file = File::create(&zip_path).expect("Failed to create zip file");
+        let response = reqwest::blocking::get(zip_url).expect("Failed to download fox zip");
         file.write_all(&response.bytes().expect("Failed to read response")).expect("Failed to write zip");
         zip_extract::extract(File::open(&zip_path).expect("Failed to open zip"), &extract_dir, true).expect("Failed to extract zip");
     }
@@ -75,30 +64,26 @@ fn main() {
     let mut source_paths = Vec::new();
     for entry in glob::glob(&format!("{}/src/*.cpp", extract_dir.display())).expect("Failed to read glob pattern") {
         match entry {
-            Ok(path) => {
-                let filename = path.file_name().unwrap().to_string_lossy();
-                if filename != "vsscanf.cpp" {
-                    source_paths.push(path);
-                };
-            }
-            Err(e) => println!("cargo:warning=Glob error: {:?}", e),
+            Ok(path) => source_paths.push(path),
+            Err(e) => eprintln!("cargo:warning=Glob error: {:?}", e),
         }
     }
+    const CAPI: &str = "cfoxtk/foxtk.cpp";
     cc::Build::new()
         .cpp(true)
-        .flag_if_supported("-std=c++14")
+        .flag_if_supported("-std=c++11")
         .flag_if_supported("/EHsc")
-        .includes([extract_dir.join("include")])
-        .files(source_paths)
         .define("WIN32", None)
-        .define("_WINDOWS", None)
-        .define("UNICODE", None)
-        .compile("fox");
-    const CAPI: &str = "cfoxtk/foxtk.cpp";
-    cc::Build::new().cpp(true).file(CAPI).compile("cfoxtk");
+        .define("HAVE_VSSCANF", None)
+        .includes([extract_dir.join("include")])
+        .include("cfoxtk")
+        .files(source_paths)
+        .file(CAPI)
+        .compile("cfoxtk");
     println!("cargo:rerun-if-changed={CAPI}");
     bindgen::Builder::default()
         .header("cfoxtk/foxtk.h")
+        .clang_args(&["-Icfoxtk".to_string(), format!("-I{}", extract_dir.join("include").display())])
         .generate()
         .expect("Unable to generate bindings")
         .write_to_file(
