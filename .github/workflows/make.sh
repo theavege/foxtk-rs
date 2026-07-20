da0fbe3546453fc31e16f3142a1f8dd1b784854b
Artem V. Ageev <artem.v.ageev@gmail.com>
2026-07-20 16:41:30 +0300

    add shfmt

--- .github/workflows/make.sh
+++ .github/workflows/make.sh
@@ -7,9 +7,9 @@ if ! (pkg-config --cflags 'fox'); then
     case ${ID:?} in
         debian | ubuntu) sudo bash -c '
             apt-get update
-            apt-get install -y libfox-1.6-dev
+            apt-get install -y shfmt libfox-1.6-dev
         ' ;;
-        fedora | alma) sudo dnf install -y fox-devel ;;
+        fedora | alma) sudo dnf install -y shfmt fox-devel ;;
     esac 1>/dev/null
 fi
 
