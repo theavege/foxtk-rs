42e4334802745ad755eeb570b8b72d8efa9ab9f3
Artem V. Ageev <artem.v.ageev@gmail.com>
2026-09-08 13:06:29 +0300

    fix make.sh

--- .github/workflows/make.sh
+++ .github/workflows/make.sh
@@ -5,13 +5,16 @@ function _setup
     if [[ -f '/etc/os-release' ]]; then
         source '/etc/os-release'
         if ! command -v fox-config >/dev/null; then
+            declare -ra DEPS=(sh{fmt,ell}check)
             case ${ID:?} in
                 debian | ubuntu) sudo bash -c '
                     apt-get update
-                    apt-get install -y shfmt shellcheck libfox-1.6-dev
+                    apt-get install -y "${DEPS[@]}" libfox-1.6-dev
                 ' ;;
-                fedora | alma) sudo dnf install -y shfmt shellcheck fox-devel ;;
+                fedora | alma) sudo dnf install -y "${DEPS[@]}" fox-devel ;;
             esac 1>/dev/null
+            shellcheck --external-sources "${0}"
+            shfmt -ci -fn -i 4 -d "${0}"
         fi
     fi
 )
@@ -22,15 +25,12 @@ if ((${#})); then
     case ${1} in
         setup) _setup ;;
         build)
-            shellcheck --external-sources "${0}"
-            shfmt -ci -fn -i 4 -d "${0}"
-
-            declare -r CSRC="foxtk-sys/src"
+            declare -r CSRC='foxtk-sys/src'
             clang-tidy "${CSRC:?}"/*.{cpp,h} -- "$(fox-config --cflags)"
             clang-format --dry-run --Werror -style=Mozilla "${CSRC:?}"/*.{cpp,h}
 
-            cargo clippy --quiet --features="all" --examples
-            cargo build --release --features="all" --examples
+            cargo clippy --quiet --features='all' --examples
+            cargo build --release --features='all' --examples
             cargo fmt --check --all
             ;;
     esac
