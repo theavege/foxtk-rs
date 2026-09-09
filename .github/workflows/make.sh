5e47ab09d6f8163a3a526321a8080ee548a7a4df
Artem Ageev <artem.v.ageev@gmail.com>
2026-09-09 17:24:16 +0300

    fix build

--- .github/workflows/make.sh
+++ .github/workflows/make.sh
@@ -5,7 +5,9 @@ function _setup
     if [[ -f '/etc/os-release' ]]; then
         source '/etc/os-release'
         if ! command -v fox-config >/dev/null; then
-            declare -ra DEPS=(sh{fmt,ell}check)
+            declare -ra DEPS=(
+                sh{fmt,ell}check
+            )
             case ${ID:?} in
                 debian | ubuntu) sudo bash -c '
                     apt-get update
