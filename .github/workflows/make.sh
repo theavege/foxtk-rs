133431b8b3f26c6a503d1efdd6e91527c76c5396
Artem Ageev <artem.v.ageev@gmail.com>
2026-09-10 17:06:26 +0300

    fmt

--- .github/workflows/make.sh
+++ .github/workflows/make.sh
@@ -26,12 +26,11 @@ if ((${#})); then
         setup) _setup ;;
         build)
             declare -ra CSRC=('foxtk-sys/src'/*.{cpp,h})
-
             clang++ -std=c++17 -Wall -Wextra -Wpedantic -O2 \
                 -fvisibility=hidden -fstack-protector-strong -fPIC \
                 "$(fox-config --cflags)" -c 'foxtk-sys/src/foxtk.cpp' -o foxtk.o
-            ar rcs libfoxtk.a foxtk.o
-
+            clang -std=c17 -Wall -Wextra -Wpedantic -O2 \
+                 -I'foxtk-sys/src' foxtk.o 'foxtk-sys/examples/simple.c'
             clang-tidy -checks='readability-*,bugprone-*,performance-*' \
                 --warnings-as-errors='*' "${CSRC[@]}" \
                 -- "$(fox-config --cflags)"
